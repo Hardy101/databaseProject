76236a9899bc2f12c71e9443d04a2121de18cb54
Eghosa Ordia <eghordia130@gmail.com>
2022-11-09 10:17:47 +0100

    Push

--- assets/includes/nav.php
+++ assets/includes/nav.php
@@ -2,7 +2,7 @@
     <div class="flex-btw">
         <h1 class="expense-h1">GrpOne</h1>
         <ul class="flex-btw nav-ul">
-            <li><a class="nav-content" href="#">Home</a></li>
+            <li><a class="nav-content" href="./index.html">Home</a></li>
             <li><a class="nav-content" href="#">About</a></li>
             <li><a class="nav-content" href="#">FAQ</a></li>
             <li><a class="nav-content" href="#">Contact</a></li>
